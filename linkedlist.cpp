8a3a31a2ad6eda5d43a2cca8a520891c7cb20134
mahfuj10 <mahfujglobal@gmail.com>
2023-03-05 14:53:01 +0600

    added merge linkedlist

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -89,6 +89,74 @@ void display(node *head)
         temp = temp->next;
     };
 };
+
+node *merge(node *head1, node *head2)
+{
+    node *p1 = head1;
+    node *p2 = head2;
+    node *dummyNode = new node(-1);
+    node *p3 = dummyNode;
+
+    while (p1 != NULL && p2 != NULL)
+    {
+        if (p1->data < p2->data)
+        {
+            p3->next = p1;
+            p1 = p1->next;
+        }
+        else
+        {
+            p3->next = p2;
+            p2 = p2->next;
+        }
+        p3 = p3->next;
+    }
+
+    while (p1 != NULL)
+    {
+        p3->next = p1;
+        p1 = p1->next;
+        p3 = p3->next;
+    }
+
+    while (p2 != NULL)
+    {
+        p2->next = p2;
+        p2 = p2->next;
+        p3 = p3->next;
+    }
+
+    return dummyNode->next;
+};
+
+node *mergeLinkedListRecursive(node *head1, node *head2)
+{
+    if (head1 == NULL)
+    {
+        return head2;
+    };
+
+    if (head2 == NULL)
+    {
+        return head1;
+    };
+
+    node *result;
+
+    if (head1->data < head2->data)
+    {
+        result = head1;
+        result = mergeLinkedListRecursive(head1->next, head2);
+    }
+    else
+    {
+        result = head2;
+        result = mergeLinkedListRecursive(head1, head2->next);
+    }
+
+    return result;
+}
+
 int main()
 {
     node *head = NULL;
