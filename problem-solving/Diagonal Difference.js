23725dd0ab4d897e910bace13af03a73a43d21bd
mahfuj10 <mahfujglobal@gmail.com>
2023-03-27 21:52:39 +0600

    Diagonal Difference

---
+++ problem-solving/Diagonal Difference.js
@@ -0,0 +1,12 @@
+function diagonalDifference(arr) {
+    // Write your code here
+    let s1 = 0;
+    let s2 = 0;
+    let size = arr.length;
+    for(let i=0; i < size; i++){
+        s1+=arr[i][i]
+        s2+=arr[i][size-i-1]
+    }
+    return Math.abs(s1 - s2)
+// return arr
+}
