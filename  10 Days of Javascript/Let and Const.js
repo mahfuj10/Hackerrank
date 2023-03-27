dc50d628287b867448101eee59fc1cae84796377
mahfuj10 <mahfujglobal@gmail.com>
2023-03-27 22:19:10 +0600

    Let and Const

---
+++  10 Days of Javascript/Let and Const.js
@@ -0,0 +1,22 @@
+
+function main() {
+    
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    let r=readLine();
+
+    const PI= Math.PI;
+    const area = parseFloat(PI * r * r);
+    const perimeter =  parseFloat(2 * PI * r); 
+    
+    console.log(area);
+    
+    console.log(perimeter);  
+    
+}
+ 
+
+ 
+    // Print the area of the circle:
+    
+    // Print the perimeter of the circle:
+
