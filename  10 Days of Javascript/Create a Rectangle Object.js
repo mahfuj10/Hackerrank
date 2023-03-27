39346bb1430bd29646baaf965760bede86a9d00e
mahfuj10 <mahfujglobal@gmail.com>
2023-03-27 22:17:43 +0600

    Create a Rectangle Object

---
+++  10 Days of Javascript/Create a Rectangle Object.js
@@ -0,0 +1,16 @@
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    const length = a;
+    const width = b;
+    const perimeter = 2 * (a + b);
+    const area = a * b;
+    return {
+        length,
+        width,
+        perimeter,
+        area
+    };
+}
+
