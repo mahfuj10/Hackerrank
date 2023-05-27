0b80b9e2b16a8792c4dff309b0693b75f9bb8dbf
mahfuj10 <mahfujglobal@gmail.com>
2023-05-27 17:27:41 +0600

    variable sized arrays

---
+++ c++/Variable Sized Arrays.cpp
@@ -0,0 +1,35 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n, q;
+    cin >> n >> q;
+    vector<int> arr[n];
+
+    for (int i = 0; i < n; ++i)
+    {
+        int m;
+        cin >> m;
+        int o;
+        for (int j = 0; j < m; ++j)
+        {
+            cin >> o;
+            arr[i].push_back(o);
+        }
+    }
+
+    int r, s;
+    for (int k = 1; k <= q; ++k)
+    {
+        cin >> r >> s;
+        cout << arr[r][s] << endl;
+    }
+
+    return 0;
+}
