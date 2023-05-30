74c68db3b8b07027c715681aa6db0e8559c1bff0
mahfuj10 <mahfujglobal@gmail.com>
2023-05-30 21:32:06 +0600

    Day 5: Loops.cpp

--- 30 Days of Code/Loops.js
+++
@@ -1,7 +0,0 @@
-function main() {
-    const n = parseInt(readLine().trim(), 10);
-    for(let i = 1; i <= 10; i++){
-        const multipy = n * i;
-        console.log(n + ' x ' + i + ' =', multipy);
-    }
-}
