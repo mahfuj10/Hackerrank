bc36e5ba5af7c47eba69041ebddf2fc21a89f445
mahfuj10 <mahfujglobal@gmail.com>
2023-06-17 20:22:37 +0600

    Maximum Draws

---
+++ Mathematics/Maximum Draws.cpp
@@ -0,0 +1,11 @@
+/*
+ * Complete the 'maximumDraws' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+int maximumDraws(int n)
+{
+    return n + 1;
+}
